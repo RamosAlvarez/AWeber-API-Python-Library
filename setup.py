010a80a4be0dd2ebe6382aab4938fc892c4c82ba
Russ P <russp@aweber.com>
2010-11-24 11:04:58 -0500

    Updated URL to point to github page

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='aweber_api',
     version='1.0.1',
     packages=find_packages(exclude=['tests']),
-    url='http://labs.aweber.com/',
+    url='https://github.com/aweber/AWeber-API-Python-Library',
     install_requires = ['oauth2 >= 1.2'],
     include_package_data=True
 )
